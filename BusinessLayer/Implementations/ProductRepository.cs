1816a433a4885e4257d34d200e866a830cd12408
androbianca <biaanca.9805@yahoo.com>
2018-11-16 08:44:06 +0200

    Repo

---
+++ BusinessLayer/Implementations/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Implementations
+{
+   public class ProductRepository : Repository
+    {
+       public ProductRepository (ApplicationContext context): base(context)
+        {
+        }
+    }
+}
---
+++ BusinessLayer/Implementations/ShoppingCartRepository.cs
@@ -0,0 +1,11 @@
+﻿using DataLayer;
+
+namespace BusinessLayer.Implementations
+{
+    public class ShoppingCartRepository : Repository
+    {
+        public ShoppingCartRepository(ApplicationContext context) : base(context)
+        {
+        }
+    }
+}
