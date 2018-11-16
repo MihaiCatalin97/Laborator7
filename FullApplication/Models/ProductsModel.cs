645da7e507762f603380a003adfc379ea13061c8
BogdanConstantin <constantin.bogdan.mirza@gmail.com>
2018-11-16 08:37:19 +0200

    Added Products and ShoppingCart Models

---
+++ FullApplication/Models/ProductsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FullApplication.Models
+{
+    public class ProductsModel
+    {
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public int Pieces { get; set; }
+
+        [Required]
+        public Guid ShoppingCartId { get; set; }
+    }
+}
---
+++ FullApplication/Models/ShoppingCartModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FullApplication.Models
+{
+    public class ShoppingCartModel
+    {
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+    }
+}
